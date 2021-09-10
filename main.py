29aa2d2ba2ce2b8140478fcd766ca29bb4757f70
MakarenkoArtem <74790725+MakarenkoArtem@users.noreply.github.com>
2021-09-10 23:39:19 +0400

    v1.0

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, request, session
+import datetime
+from os import listdir, remove, rmdir, mkdir, environ
+app = Flask(__name__)
+app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=1)
+app.config['SECRET_KEY'] = 'secret_key'
+@app.route('/')
+def main_list():  # форма для регистрации
+    return render_template('index.html')
+
+
+def main():
+    try:
+        mkdir("static/img")
+    except FileExistsError:
+        pass
+    if 'HEROKU' in environ:
+        port = int(environ.get("PORT", 5000))
+        print(port)
+        app.run(host='0.0.0.0', port=port)
+    else:
+        app.run(port=8080, host='127.0.0.1', debug=False)
+
+
+if __name__ == "__main__":
+    main()
